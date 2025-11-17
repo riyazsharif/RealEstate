21c5a3a427979e39b15a91b12ccf56bf92070999
riyaz sharif <riyazsharif2002@gmail.com>
2025-11-17 11:29:27 +0530

    upgrade completed: next13 + react18

--- next.config.js
+++ next.config.js
@@ -2,18 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      crypto: false,
-      stream: require.resolve("stream-browserify"),
-      path: false,
-      fs: false,
-    };
-    return config;
-  },
-};
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+  }
+}
 
 module.exports = nextConfig;
