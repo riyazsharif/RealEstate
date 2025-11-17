faf12cb0d00ab350c2ef050ee6217c6707ae5a2d
riyaz sharif <riyazsharif2002@gmail.com>
2025-11-17 11:09:01 +0530

    fix: final merged next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  images:{
-    domains:['bayut-production.s3.eu-central-1.amazonaws.com']
-},
-}
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
