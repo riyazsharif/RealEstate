dfe2d489d2b5a9c53de0e828718da294fb261f4d
riyaz sharif <riyazsharif2002@gmail.com>
2025-11-17 11:43:56 +0530

    fix: removed invalid chakra imports

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,5 +1,5 @@
-import { Box, Flex, Spacer, Text } from '@chakra-ui/layout';
-import { Avatar } from '@chakra-ui/avatar';
+import { Box, Flex, Spacer, Text } from '@chakra-ui/react';
+import { Avatar } from '@chakra-ui/react';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
